37ff38d7e4f4d173a10ee3cf761928c81b66238c
Karan07017 <karanarya7875@gmail.com>
2026-07-27 20:13:34 +0530

    meet logo redirect added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 import ProfilePhoto from './ProfilePhoto'
 
 const NavBar = () => {
     return (
         <div className='w-full h-16 sticky top-0 z-20 glass-panel border-x-0 border-t-0 flex items-center justify-center shadow-lg shadow-black/20'>
             <div className='px-5 sm:px-8 flex items-center w-full justify-between max-w-[1400px]'>
-                <div className='text-3xl sm:text-4xl font-extrabold gradient-text tracking-tight'>MEET</div>
+                <Link href='/' className='text-3xl sm:text-4xl font-extrabold gradient-text tracking-tight cursor-pointer hover:opacity-80 transition-opacity duration-200'>MEET</Link>
                 <ProfilePhoto />
             </div>
         </div>
